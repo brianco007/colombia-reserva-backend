74693731971a1be2e44a963d13124e97855673a0
Brian Cordoba <briancormin@gmail.com>
2024-10-15 18:57:44 -0500

    Added NodeMailer Env Variables

--- nodemailerTransporter.js
+++ nodemailerTransporter.js
@@ -1,10 +1,11 @@
 import nodemailer from "nodemailer"
+import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
   service: 'gmail', // Puedes cambiar esto según tu proveedor de correo
   auth: {
-    user: 'briancormin@gmail.com', // Tu correo
-    pass: 'fdfeyevwgmvfwssd', // Contraseña o App Password si usas Gmail con 2FA
+    user: process.env.NODEMAILER_USER, 
+    pass: process.env.NODEMAILER_PASS, // Contraseña o App Password si usas Gmail con 2FA
   },
 });
 
