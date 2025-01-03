c8480e1a8ab4f8c99b7ac03e94181989a13de2e1
Brian Cordoba <briancormin@gmail.com>
2025-01-03 11:18:38 -0500

    add role employee to login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,56 +1,66 @@
-import bcrypt from 'bcrypt'; 
-import signupModel from "../models/signupModel.js"
-import { tokenGenerator } from '../tools/tokenGenerator.js';
+import bcrypt from "bcrypt";
+import signupModel from "../models/signupModel.js";
+import employeesModel from "../models/employeesModel.js";
+import { tokenGenerator } from "../tools/tokenGenerator.js";
 import "dotenv/config";
 
-
 const loginController = {
-  userLogin: async(req, res)=>{
+  userLogin: async (req, res) => {
     try {
-      const { email,  password } = req.body;
-      const userFound = await signupModel.findOne({email})//finds the user in Mongo DB
+      const { email, password } = req.body;
+      const userFound = await signupModel.findOne({ email }); //finds the user in Mongo DB
 
       //superadmin
-      if(email === process.env.SUPERADMIN_EMAIL){
+      if (email === process.env.SUPERADMIN_EMAIL) {
         const isTheSame = await bcrypt.compare(password, userFound.password);
         if (isTheSame) {
           const token = await tokenGenerator({
             id: userFound._id,
             email: userFound.email,
             businessId: userFound.businessId,
-            role: "superadmin"
-          })
-          res.json(token)
+            role: "superadmin",
+          });
+          res.json(token);
         } else {
-          res.json({message: 'User or password is wrong.'});
+          res.json({ message: "User or password is wrong." });
         }
       }
       // Other admins
       else {
-        if(email){ // if there is an email in the DB
+        if (email) {
+          // if the email exist in the database
+          const employeeFound = await employeesModel.findOne({ email }); //finds if user is employee
           const isTheSame = await bcrypt.compare(password, userFound.password);
+
           if (isTheSame) {
-            const token = await tokenGenerator({
-              id: userFound._id,
-              email: userFound.email,
-              businessId: userFound.businessId,
-              role: "admin"
-            })
-            res.json(token)
+            if (employeeFound) {
+              const token = await tokenGenerator({
+                id: employeeFound._id,
+                email: employeeFound.email,
+                businessId: employeeFound.businessId,
+                role: "employee",
+              });
+              res.json(token);
+            } else {
+              const token = await tokenGenerator({
+                id: userFound._id,
+                email: userFound.email,
+                businessId: userFound.businessId,
+                role: "admin",
+              });
+              res.json(token);
+            }
           } else {
-            res.json({message: 'User or password is wrong.'});  
+            res.json({ message: "User or password is wrong." });
           }
         } else {
-          res.json({message: 'User or password is wrong.'});
+          res.json({ message: "User or password is wrong." });
         }
       }
-      
-     
     } catch (err) {
-      res.json({message: 'An error ocurred', error: err.message})
+      res.json({ message: "An error ocurred", error: err.message });
     }
-  }
-}
-
+  },
+};
 
 export default loginController;
--- models/employeesModel.js
+++ models/employeesModel.js
@@ -16,6 +16,19 @@ const employeesModel = new Schema({
   schedule: {type: [scheduleModel], _id: false, required: [true, "Debe subir el horario de al menos un día."]},
   timeSlots: {type: [{day: String, timeSlots: [{value: String, display: String, _id: false}]}], _id: false},
   sessionTime: {type: String},
+  phone: {type: String, trim: true, required: [true, "The employee's phone number is required."]},
+  email: { 
+    type: String, 
+    required: [true, "The employee's email is required."],
+    trim: true,
+    validate: {
+      validator: function(value){
+        return emailRegex.test(value)
+      },
+      message: "Email is not valid"
+    }
+
+  },
   active: {type: Boolean, required: [true, "The employee's status (active) is required."]}
 },
 {versionKey:false , timestamps: false}
